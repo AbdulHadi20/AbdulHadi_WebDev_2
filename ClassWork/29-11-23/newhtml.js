bf67a38422fe9b6ca15a343c2462ce71c45648e0
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-11-29 20:58:24 +0400

    ...

---
+++ ClassWork/29-11-23/newhtml.js
@@ -0,0 +1,20 @@
+anime ({
+    targets: '.square',
+    left: 500,
+    duration: 600,
+    easing: 'easeOutElastic(1, 0.5)', 
+})
+
+anime ({
+    targets: '.square2',
+    left: 500,
+    duration: 600, 
+    easing: 'spring',
+})
+
+anime ({
+    targets: '.square3', 
+    left: 500,
+    duration: 600,
+    easing: 'easeOutBounce',
+})
